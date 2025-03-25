00e5d4e716b409ff9d82be776ef4809c0601f1c4
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-03-24 21:54:15 -0600

    adding new pages to the db wit form

--- favoritos/views.py
+++ favoritos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Favoritos
 
 # Create your views here.
 
@@ -6,4 +7,9 @@ def index_favoritos(request):
     return render(request, 'index.html')
 
 def crear_favoritos(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        url = request.POST['url']
+        Favoritos.objects.create(nombre=nombre, url=url)
+       
     return render(request, 'favoritos/crear.html')
